02cee15531f1bc4793fa3b2dc134fd938c13aa0b
faryal16 <162690320+faryal16@users.noreply.github.com>
2024-03-23 16:38:42 +0500

    my 1st simple calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "subtruction", "multiplication", "division"],
+    },
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtruction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select a valid operator");
+}
